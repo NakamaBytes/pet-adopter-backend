80605d94c892f4eca9c10d6a7f04681153f64ff4
RaihanArdianata <raihan.ardianata@gmail.com>
2024-09-01 20:59:36 +0700

    perbaikan response get

--- src/controllers/v1/pet/pet.controller.ts
+++ src/controllers/v1/pet/pet.controller.ts
@@ -1,22 +1,26 @@
 import { catchAsync } from "@utils/catchAsync";
 import { get as getPets, find as findPet, create as createPet, update as updatePet, remove as removePet } from "@services/v1/pet.service";
 import { petCreateValidatorType } from "@validator/v1/pet.validator";
+import { map } from "lodash";
 
 export const get = catchAsync(async (c) => {
 
-  console.log("pet");
-
   const result = await getPets();
+  const adoptions = map(result, ({ adoptions, shelter, ...item }) => ({ ...item, adoptions: { ...adoptions?.[0]?.user, isCurrentUser: adoptions?.[0]?.isCurrentUser, createdAt: adoptions?.[0]?.createdAt, updatedAt: adoptions?.[0]?.updatedAt }, shelter: { ...shelter?.[0]?.shelter, isCurrentShelter: shelter?.[0]?.isCurrentShelter, createdAt: shelter?.[0]?.createdAt, updatedAt: shelter?.[0]?.updatedAt } }));
 
-  return c.json({ data: result });
+
+  return c.json({ data: adoptions });
 });
 
 export const find = catchAsync(async (c) => {
 
   const petId = c.req.param("petId");
   const result = await findPet({ id: petId });
+  const adoptions = map(result?.adoptions, ({ user, status, isCurrentUser, createdAt, updatedAt }) => ({ ...user, adoptionStatus: status, isCurrentUser, createdAt, updatedAt }));
+  const shelter = map(result?.shelter, ({ shelter, status, isCurrentShelter, createdAt, updatedAt }) => ({ ...shelter, status: status, isCurrentShelter, createdAt, updatedAt }));
 
-  return c.json({ data: result });
+
+  return c.json({ data: { ...result, adoptions, shelter } });
 });
 
 export const create = catchAsync(async (c) => {
